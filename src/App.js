fa63bbe1a5b696d7887ef79d3927c4f70d1fe951
vanz1220 <ije.malong@gmail.com>
2022-03-24 17:11:55 +0800

    init

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, Im Ivan Jan Malong.
+        </p>
+        <p>
+          This is my first React Js.
         </p>
         <a
           className="App-link"
