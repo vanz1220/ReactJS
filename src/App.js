13d114d278c99250aa875c949a19c2ea53b17af1
vanz1402 <ije.malong@gmail.com>
2022-03-28 13:40:12 +0800

    awit na awit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import SocialFollow from "./SocialFollow"
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <SocialFollow />
     </div>
   );
 }
