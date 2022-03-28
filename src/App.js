1463d6f8e8c853eb13f63756dafeb3289e469686
vanz1402 <ije.malong@gmail.com>
2022-03-28 17:01:07 +0800

    awitized

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 import SocialFollow from "./SocialFollow"
+import About from "./About"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Container, NavDropdown, Nav} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          This is my first React Js.
-        </p>
-      </header>
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand>
+            <img
+            alt=""
+            src={logo}
+            width="50"
+            height="50"
+            className="d-inline-block align-middle"
+            />
+            EZM Buraot
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link>Home</Nav.Link>
+                <Nav.Link>About</Nav.Link>
+                <Nav.Link>Contact</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <About />
       <footer><SocialFollow /></footer>
     </div>
   );
--- src/SocialFollow.js
+++ src/SocialFollow.js
@@ -13,18 +13,18 @@ export default function SocialFollow() {
     <h3>Social Follow</h3>
     <a href="https://www.youtube.com/"
         className="youtube social">
-        <FontAwesomeIcon icon={faYoutube} size="2x" />
+        <FontAwesomeIcon icon={faYoutube} size="1x" />
       </a>
       <a href="https://www.facebook.com/"
         className="facebook social">
-        <FontAwesomeIcon icon={faFacebook} size="2x" />
+        <FontAwesomeIcon icon={faFacebook} size="1x" />
       </a>
       <a href="https://www.twitter.com/" className="twitter social">
-        <FontAwesomeIcon icon={faTwitter} size="2x" />
+        <FontAwesomeIcon icon={faTwitter} size="1x" />
       </a>
       <a href="https://www.instagram.com/"
         className="instagram social">
-        <FontAwesomeIcon icon={faInstagram} size="2x" />
+        <FontAwesomeIcon icon={faInstagram} size="1x" />
       </a>
     </div>
   );
