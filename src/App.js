f8e0ef09d106e3b2ee8296a227b41025c2d527da
vanz1402 <ije.malong@gmail.com>
2022-03-30 14:44:08 +0800

    owww

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             height="50"
             className="d-inline-block align-middle"
             />
-            OWSHII&nbsp;&nbsp;✌😉
+            OWSHII&nbsp;&nbsp;✌😉 
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
