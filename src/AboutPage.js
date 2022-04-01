ee280ad3336b27205dd0de3e41d068822542e575
vanz1220 <92617472+vanz1220@users.noreply.github.com>
2022-04-01 23:16:49 +0800

    random fade in up

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -8,23 +8,23 @@ export default function AboutPage() {
     <Random
       text="It is not our differences that divide us."
       iterations={1}
-          effect="verticalFadeIn"
-          effectChange={2}
-          effectDirection="up"
+      effect="verticalFadeIn"
+      effectChange={2}
+      effectDirection="up"
     />
     <Random
       text="It is our inability to recognize, accept,"
       iterations={1}
-          effect="verticalFadeIn"
-          effectChange={2}
-          effectDirection="up"
+      effect="verticalFadeIn"
+      effectChange={2}
+      effectDirection="up"
     />
     <Random
       text="and celebrate those differences. - Audre Lorde"
       iterations={1}
-          effect="verticalFadeIn"
-          effectChange={2}
-          effectDirection="up"
+      effect="verticalFadeIn"
+      effectChange={2}
+      effectDirection="up"
     />
     </h1>
   </div>
