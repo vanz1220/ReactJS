e61494b5ae2f1f09626813900640906b8c4a1401
vanz1220 <92617472+vanz1220@users.noreply.github.com>
2022-04-01 18:47:01 +0800

    random fade in up

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -1,67 +1,31 @@
 import React from 'react'
-import AnimatedText from 'react-animated-text-content';
+import { Random } from 'react-animated-text';
 
 export default function AboutPage() {
   return (
     <div class="About-Height">
     <h1 class="wew">
-      <AnimatedText
-        type="chars" // animate words or chars
-        animation={{
-          x: '200px',
-          y: '-20px',
-          scale: 1.1,
-          ease: 'ease-in-out',
-        }}
-        animationType="diagonal"
-        interval={0.2}
-        duration={2.5}
-        tag="p"
-        className="animated-paragraph"
-        includeWhiteSpaces
-        threshold={0.1}
-        rootMargin="20%"
-      >
-        It is not our differences that divide us.
-      </AnimatedText>
-      <AnimatedText
-        type="chars" // animate words or chars
-        animation={{
-          x: '200px',
-          y: '-20px',
-          scale: 1.1,
-          ease: 'ease-in-out',
-        }}
-        animationType="diagonal"
-        interval={0.2}
-        duration={2.5}
-        tag="p"
-        className="animated-paragraph"
-        includeWhiteSpaces
-        threshold={0.1}
-        rootMargin="20%"
-      >
-      It is our inability to recognize, accept,
-      </AnimatedText>
-      <AnimatedText
-        type="chars" // animate words or chars
-        animation={{
-          x: '200px',
-          y: '-20px',
-          scale: 1.1,
-          ease: 'ease-in-out',
-        }}
-        animationType="diagonal"
-        interval={0.2}
-        duration={2.5}
-        tag="p"
-        className="animated-paragraph"
-        includeWhiteSpaces
-        threshold={0.1}
-        rootMargin="20%"
-      >
-      and celebrate those differences. - Audre Lorde
-      </AnimatedText>
+    <Random
+      text="It is not our differences that divide us."
+      iterations={1}
+          effect="verticalFadeIn"
+          effectChange={2}
+          effectDirection="up"
+    />
+    <Random
+      text="It is our inability to recognize, accept,"
+      iterations={1}
+          effect="verticalFadeIn"
+          effectChange={2}
+          effectDirection="up"
+    />
+    <Random
+      text="and celebrate those differences. - Audre Lorde"
+      iterations={1}
+          effect="verticalFadeIn"
+          effectChange={2}
+          effectDirection="up"
+    />
     </h1>
   </div>
   )
