4e1b47d4431d3cee0eaf265536c5d2e227e627e5
vanz1220 <92617472+vanz1220@users.noreply.github.com>
2022-04-01 09:11:01 +0800

    qoute

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -6,7 +6,7 @@ export default function AboutPage() {
     <div class="About-Height">
     <h1 class="wew">
       <AnimatedText
-        type="words" // animate words or chars
+        type="chars" // animate words or chars
         animation={{
           x: '200px',
           y: '-20px',
@@ -23,8 +23,44 @@ export default function AboutPage() {
         rootMargin="20%"
       >
         It is not our differences that divide us.
-        It is our inability to recognize, accept,
-        and celebrate those differences. - Audre Lorde
+      </AnimatedText>
+      <AnimatedText
+        type="chars" // animate words or chars
+        animation={{
+          x: '200px',
+          y: '-20px',
+          scale: 1.1,
+          ease: 'ease-in-out',
+        }}
+        animationType="diagonal"
+        interval={0.2}
+        duration={2.5}
+        tag="p"
+        className="animated-paragraph"
+        includeWhiteSpaces
+        threshold={0.1}
+        rootMargin="20%"
+      >
+      It is our inability to recognize, accept,
+      </AnimatedText>
+      <AnimatedText
+        type="chars" // animate words or chars
+        animation={{
+          x: '200px',
+          y: '-20px',
+          scale: 1.1,
+          ease: 'ease-in-out',
+        }}
+        animationType="diagonal"
+        interval={0.2}
+        duration={2.5}
+        tag="p"
+        className="animated-paragraph"
+        includeWhiteSpaces
+        threshold={0.1}
+        rootMargin="20%"
+      >
+      It is our inability to recognize, accept,
       </AnimatedText>
     </h1>
   </div>
